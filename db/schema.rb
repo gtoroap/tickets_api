5f2c67ce42ad1861fc5b0967841784c42fa15cf3
Gustavo Toro <gtoroap@gmail.com>
2019-05-14 06:49:56 -0500

    Added some seeds for users and tickets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_14_113326) do
+ActiveRecord::Schema.define(version: 2019_05_14_114425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2019_05_14_113326) do
     t.datetime "valid_until"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_tickets_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -36,4 +38,5 @@ ActiveRecord::Schema.define(version: 2019_05_14_113326) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "tickets", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Creating users..."
+User.create(username: 'gtoroap', pin_code: '1234', name: 'Gustavo', last_name: 'Toro', email: 'gtoroap@gmail.com')
+puts "Creating users... Done"
+
+puts "Creating tickets..."
+Ticket.create(
+  [
+    {
+      user_id: 1, venue: 'Cinema #1', seat: 'A1', location: 'Section A', price: 5, valid_from: Time.now, valid_until: Time.now + 24.hours
+    },
+    {
+      user_id: 1, venue: 'Cinema #1', seat: 'A2', location: 'Section A', price: 5, valid_from: Time.now, valid_until: Time.now + 24.hours
+    },
+    {
+      user_id: 1, venue: 'Cinema #1', seat: 'A3', location: 'Section A', price: 5, valid_from: Time.now, valid_until: Time.now + 24.hours
+    },
+    {
+      user_id: 1, venue: 'Cinema #1', seat: 'A4', location: 'Section A', price: 5, valid_from: Time.now, valid_until: Time.now + 24.hours
+    }
+  ]
+)
+puts "Creating tickets... Done"
